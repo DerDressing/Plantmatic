e252267cf25728221437e6073e25ac5422b81a6d
Dominic <m@m.de>
2025-04-03 19:22:27 +0200

    a

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,13 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("com.pi4j:pi4j-ktx:2.4.0") // Kotlin DSL
+    implementation("com.pi4j:pi4j-core:2.3.0")
+    implementation("com.pi4j:pi4j-plugin-raspberrypi:2.3.0")
+    implementation("com.pi4j:pi4j-plugin-pigpio:2.3.0")
+    //console DSL to log with SLF4J
+    implementation("org.slf4j:slf4j-api:1.7.32")
+    implementation("org.slf4j:slf4j-simple:1.7.32")
 }
 
 tasks.test {
